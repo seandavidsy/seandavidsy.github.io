2345617d5756d3a50501a033855137187bcd0d4b
seandavidsy <seandavidsy@gmail.com>
2022-07-20 15:01:16 -0400

    First Update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Main">
+      <header id="home">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <nav id="nav-wrap" class="nav-wrap">
+         <ul id="nav" class="nav">
+            <li><a class="smoothscroll" href="#home">Home</a></li>
+            <li><a class="smoothscroll" href="#about">About</a></li>
+	          <li><a class="smoothscroll" href="#resume">Resume</a></li>
+            <li><a class="smoothscroll" href="#portfolio">Projects</a></li>
+            <li><a class="smoothscroll" href="#activities">Activities</a></li>
+            <li><a class="smoothscroll" href="#stats">Statistics</a></li>
+            <li><a class="smoothscroll" href="#contact">Contact</a></li>
+         </ul>
+         <hr/>
+        </nav>
+        <div class="row banner">
+         <div class="banner-text">
+            <h1 class="responsive-headline">Sean Sy</h1>
+            <h3><span>Software Engineer</span></h3>
+            <hr />
+         </div>
+      </div>
       </header>
+      
     </div>
+    
+    
   );
 }
 
